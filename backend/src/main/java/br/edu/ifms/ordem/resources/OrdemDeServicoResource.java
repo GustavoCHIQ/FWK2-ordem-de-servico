4025dcb6dd4a2d9b19a82fd1465ebcf291aef504
Gustavo Chiquitin <53371023+GustavoCHIQ@users.noreply.github.com>
2022-08-05 22:00:35 -0400

    Correção listagem de equipamento
    Correção para mostrar os equipamentos da ordem de serviço

--- backend/src/main/java/br/edu/ifms/ordem/resources/OrdemDeServicoResource.java
+++ backend/src/main/java/br/edu/ifms/ordem/resources/OrdemDeServicoResource.java
@@ -1,10 +1,12 @@
 package br.edu.ifms.ordem.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,7 +28,7 @@ public class OrdemDeServicoResource {
 	@Autowired
 	private OrdemDeServicoService service;
 
-	/*@GetMapping
+	@GetMapping
 	public ResponseEntity<Page<OrdemDeServicoDTO>> findAllPaged(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
@@ -38,12 +41,6 @@ public class OrdemDeServicoResource {
 		
 		Page<OrdemDeServicoDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
-	}*/
-	
-	@GetMapping
-	public ResponseEntity<List<OrdemDeServicoDTO>> findAll(){
-		List<OrdemDeServicoDTO> lista = service.findAll();
-		return ResponseEntity.ok().body(lista);
 	}
 	
 	@GetMapping(value = "/{id}")
