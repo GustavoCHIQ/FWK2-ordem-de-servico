fb6e2046dac7f952d37e2e96b16a89d7a5fd9b63
Gustavo Chiquitin <luis.gustavo09@hotmail.com>
2022-08-19 21:01:17 -0400

    Enable CORS Origin

--- backend/src/main/java/br/edu/ifms/ordem/SysOrdemServicoApplication.java
+++ backend/src/main/java/br/edu/ifms/ordem/SysOrdemServicoApplication.java
@@ -2,12 +2,21 @@ package br.edu.ifms.ordem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SysOrdemServicoApplication {
-
+@EnableWebMvc
+public class SysOrdemServicoApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(SysOrdemServicoApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedOrigins("*");
+	}
 }
