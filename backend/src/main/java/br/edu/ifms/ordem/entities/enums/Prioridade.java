093ba53277f90974c2a1198b56bc488dae582377
Gustavo Chiquitin <53371023+GustavoCHIQ@users.noreply.github.com>
2022-08-23 13:50:10 -0400

    Delete backend directory

--- backend/src/main/java/br/edu/ifms/ordem/entities/enums/Prioridade.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifms.ordem.entities.enums;
-
-public enum Prioridade {
-	BAIXA, MEDIA, ALTA;
-}
--- backend/src/main/java/br/edu/ifms/ordem/entities/enums/Status.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifms.ordem.entities.enums;
-
-public enum Status {
-	PENDENTE, CANCELADO, EFETIVADO;
-}
