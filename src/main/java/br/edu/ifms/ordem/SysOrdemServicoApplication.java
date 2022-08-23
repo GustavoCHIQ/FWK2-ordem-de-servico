a48c5cfd24ed6a0054a9e9a95a42973d9864b30c
Gustavo Chiquitin <53371023+GustavoCHIQ@users.noreply.github.com>
2022-08-23 13:49:54 -0400

    Mudança de pastas

---
+++ src/main/java/br/edu/ifms/ordem/SysOrdemServicoApplication.java
@@ -0,0 +1,22 @@
+package br.edu.ifms.ordem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableWebMvc
+public class SysOrdemServicoApplication implements WebMvcConfigurer {
+	public static void main(String[] args) {
+		SpringApplication.run(SysOrdemServicoApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedOrigins("*");
+	}
+}
